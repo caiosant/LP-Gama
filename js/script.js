41c0695c6e0b870066f06d351f1ff674fa019a58
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2021-07-16 00:23:41 -0300

    Correção contador

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function countDown() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    document.getElementById("clock").innerHTML = `${days} + "d " + ${hours} + "h " + ${minutes} + "m " + ${seconds} + s`;
+    document.getElementById("clock").innerHTML = `${days} dias ,${hours} horas, ${minutes} minutos e ${seconds} segundos;
 
     setTimeout(countDown, 1000);
 
